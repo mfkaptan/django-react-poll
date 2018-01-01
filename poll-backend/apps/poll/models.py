79c0a3807ee7dbf43c62045267731c93b157c18e
mfkaptan <kaptanmf@gmail.com>
2018-01-01 12:58:46 +0300

    Keep questions ordered

--- poll-backend/apps/poll/models.py
+++ poll-backend/apps/poll/models.py
@@ -4,8 +4,14 @@ from django.db import models
 class Question(models.Model):
     text = models.CharField(max_length=400)
 
+    class Meta:
+        ordering = ["-pk"]
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="choices", blank=True)
     text = models.CharField(max_length=200)
     votes = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ["-pk"]
